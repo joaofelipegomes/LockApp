5f3bb6d131570c2612e6af9876115173a6f0fe45
Joao Gomes <contato@joaofelipegomes.com.br>
2024-04-28 15:26:31 -0300

    Initial commit

---
+++ unlock/Controllers/SecurityController.swift
@@ -0,0 +1,8 @@
+//
+//  SecurityController.swift
+//  unlock
+//
+//  Created by João Gomes on 28/04/24.
+//
+
+import Foundation
---
+++ unlock/LockedView.swift
@@ -0,0 +1,18 @@
+//
+//  LockedView.swift
+//  unlock
+//
+//  Created by João Gomes on 28/04/24.
+//
+
+import SwiftUI
+
+struct LockedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LockedView()
+}
